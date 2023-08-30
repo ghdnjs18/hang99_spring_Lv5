680501d43a9e0aa8fd8751342902167002d5a7d7
ghdnjs18 <ghdnjs18@naver.com>
2023-08-30 21:02:15 +0900

    코드 정리

--- src/main/java/com/sparta/springboottest/entity/User.java
+++ src/main/java/com/sparta/springboottest/entity/User.java
@@ -17,13 +17,11 @@ import org.hibernate.validator.constraints.NotBlank;
 public class User {
     @Id
     @Column
-//    @Pattern(regexp = "[a-z0-9]{4,10}", message = "유저 형식을 확인하세요.")
     @NotBlank(message = "이름은 필수 값 입니다.")
     private String username;
 
     @Column(nullable = false)
     @NotBlank(message = "비밀번호는 필수 값 입니다.")
-//    @Pattern(regexp = "[a-zA-Z0-9]{8,15}", message = "비밀번호 형식을 확인하세요.")
     private String password;
 
     public User(String username, String password) {
