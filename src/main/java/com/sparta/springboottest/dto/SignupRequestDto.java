7f40f9c753f3d96be69bb6acf1699d5e5092d5c5
ghdnjs18 <ghdnjs18@naver.com>
2023-09-04 14:15:19 +0900

    공통 예외 처리 메시지 구현

--- src/main/java/com/sparta/springboottest/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springboottest/dto/SignupRequestDto.java
@@ -10,10 +10,10 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @Size(min = 4, max = 10, message = "username은 4글자 이상, 10글자 이하입니다.", groups = SizeCheckGroup.class)
-    @Pattern(regexp = "[a-z0-9]", message = "username형식은 알파벳 소문자와 숫자만 가능합니다.", groups = PatternCheckGroup.class)
+    @Pattern(regexp = "^[a-z0-9]*$", message = "username형식은 알파벳 소문자와 숫자만 가능합니다.", groups = PatternCheckGroup.class)
     private String username;
 
     @Size(min = 8, max = 15, message = "password은 8글자 이상, 15글자 이하입니다.", groups = SizeCheckGroup.class)
-    @Pattern(regexp = "[a-zA-Z0-9]", message = "password형식은 영문, 숫자, 특수문자만 가능합니다.", groups = PatternCheckGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "password형식은 영문, 숫자, 특수문자만 가능합니다.", groups = PatternCheckGroup.class)
     private String password;
 }
--- src/main/java/com/sparta/springboottest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/springboottest/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.sparta.springboottest.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,14 +11,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RestApiException> handleValidationExceoption(MethodArgumentNotValidException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<RestApiException> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError fieldError = bindingResult.getFieldError();
+
+        RestApiException restApiException = new RestApiException(fieldError.getDefaultMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(
                 // HTTP body
                 restApiException,
                 // HTTP status code
                 HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<RestApiException> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(restApiException, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<RestApiException> nullPointerExceptionHandler(NullPointerException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(restApiException, HttpStatus.BAD_REQUEST);
     }
 }
